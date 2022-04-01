a75a52c8d375da0d65cffdd4fa25c8bb1afbb45a
Arthur Ng <43798823+arthurhohinng@users.noreply.github.com>
2022-03-31 23:49:13 -0400

    [IN-86] Add assign container script (#72)

--- restaurant/assign_container.php
+++ restaurant/assign_container.php
@@ -5,16 +5,17 @@ function get_customer_id_by_order_id( $order_id ) {
 		
 	$order = wc_get_order( $order_id );
 	$customer_id = ( $order != null ) ? ( $order->get_customer_id() ) : (null);
-	
+
 	return $customer_id;
 }
 
 // Function for when container is scanned
 function assign_container( $container_id, $order_id ) {
+	global $wpdb;
 	
 	$customer_id = get_customer_id_by_order_id( $order_id );
-	date_default_timezone_set('Canada/Toronto');
-	$date = date('m/d/Y h:i:s a', time());
+	date_default_timezone_set('America/Toronto');
+	$date = date('Y/m/d h:i:s', time());
 	$query = $wpdb->prepare("UPDATE `Container` 
 							 SET `recipient_id` = %d, `order_id` = %d, 
 							 `container_status` = 1, `transaction_date` = %s
@@ -24,4 +25,18 @@ function assign_container( $container_id, $order_id ) {
 	
 }
 
-add_shortcode ( 'assign_container', 'assign_container' );
+function assign_container_script() {
+	session_start();
+	if ( isset( $_SESSION["curr_order"] ) ) {
+		$order_id = $_SESSION["curr_order"];
+		$container_id = explode( "-", $_SERVER['REQUEST_URI'] )[1];
+		$container_id = intval( substr( $container_id, 0, 6 ) );
+		
+		echo $order_id;
+		assign_container( $container_id, $order_id );
+		
+	}
+	
+}
+
+add_shortcode ( 'assign_container_script', 'assign_container_script' );
