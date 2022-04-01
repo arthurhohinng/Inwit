efdae325632aa6055c37a29f5c66547c31123bd6
rayhanFazal <65515712+rayhanFazal@users.noreply.github.com>
2022-03-31 20:01:00 -0400

    [IN-99] Fix display order containers to show on restaurant page (#67)

--- restaurant/show_containers_for_each_order.php
+++ restaurant/show_containers_for_each_order.php
@@ -1,20 +1,28 @@
-add_shortcode('display_containers_associated_with_all_orders', 'display_containers_associated_with_all_orders');
-// this function is for the restaurant view
-function display_containers_associated_with_all_orders(){
-	global $wpdb;
-	
-	if ( is_user_logged_in() ){
-		$user = wp_get_current_user();
-		$query = $wpdb->prepare("SELECT * 
+<div class="" style="width:100%">
+                <div class="dokan-panel dokan-panel-default">
+                    <div class="dokan-panel-heading"><strong><?php esc_html_e( 'Containers for your order', 'dokan-lite' ); ?></strong></div>
+                    <div class="dokan-panel-body" id="dokan-order-notes">
+                        <?php 
+                        $user = wp_get_current_user();
+                        $container_statuses = array("Active", "Pending", "Lost", "Broken", "Purchased");
+                        $query = $wpdb->prepare("SELECT * 
 								FROM `Container` 
-								WHERE `container_status` = 1
-								AND `restaurant_id` = %d"
+								WHERE `restaurant_id` = %d
+								AND order_id = {$order_id}"
 							, $user->ID );
-		$results = $wpdb->get_results($query);
-		echo "<div> Containers for your orders </div>";
-		foreach($results as $val) {
-			echo "<div> Container ID: " . $val->container_id . "</div>";
-			echo "<div> Order #" . strval( $val->order_id ) . "</div>";
-		}
-	}
-}
+		                $results = $wpdb->get_results($query);
+		                echo '<table><thead><tr>';
+		                echo '<th>'.'Container ID'.'</th>';
+		                echo '<th>'.'Container Status'.'</th>';
+		                echo '</tr></thead><tbody>';
+		                foreach($results as $val) {
+			                echo '<tr>';
+			                echo '<td>' . $val->container_id . '</td>';
+			                echo '<td>' . $container_statuses[$val->container_status] . '</td>';
+			                echo '</tr>';
+		                }
+		                echo '</tbody></table>';
+                        ?>
+                    </div>
+                </div>
+        </div>
