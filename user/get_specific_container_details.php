79503ce34b791874f3430866140fedb9909889ee
rayhanFazal <65515712+rayhanFazal@users.noreply.github.com>
2022-03-31 22:22:20 -0400

    [IN-89]  Add description mapping for container status (#70)

--- user/get_specific_container_details.php
+++ user/get_specific_container_details.php
@@ -5,7 +5,7 @@
 
 function get_user_container_details(){
 	global $wpdb;
-	
+	$container_statuses = array("Active", "Pending", "Lost", "Broken", "Purchased");
 	if ( is_user_logged_in() ){
 		if ( isset( $_GET["id"] ) ){
 			$container_id = $_GET["id"];
@@ -18,7 +18,7 @@ function get_user_container_details(){
 			$result = $wpdb->get_row($query);
 			if ( count( $result ) > 0 ){
 				echo "<div> Container ID: " . $container_id . "</div>";
-				echo "<div> Container Status: " . strval($result->container_status) . "</div>";
+				echo "<div> Container Status: " . $container_statuses[strval($result->container_status)] . "</div>";
 				echo "<div> Order #" . strval( $result->order_id ) . "</div>";
 				echo "<a href='../product/container/?id=$container_id&option=buy'>
 						<button>Buy Container</button>
