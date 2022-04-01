c50bc25e185b3e55f0e90fe28d60c2b867da099d
Sabih S <67878058+kleenkanteen@users.noreply.github.com>
2022-04-01 03:04:52 -0400

    [IN-100] Make dropdown menu for changing order status show by default and remove update and cancel buttons (#73)

--- restaurant/edit_order_status.php
+++ restaurant/edit_order_status.php
@@ -1,21 +1,25 @@
-<li class="dokan-hide">
+<li>
+                                <span><?php esc_html_e( 'Order Status:', 'dokan-lite' ); ?></span>
+                                <label class="dokan-label dokan-label-<?php echo esc_attr( dokan_get_order_status_class( dokan_get_prop( $order, 'status' ) ) ); ?>"><?php echo esc_html( dokan_get_order_status_translated( dokan_get_prop( $order, 'status' ) ) ); ?></label>
+
+                                <?php if ( current_user_can( 'dokan_manage_order' ) && dokan_get_option( 'order_status_change', 'dokan_selling', 'on' ) == 'on' && $order->get_status() !== 'cancelled' && $order->get_status() !== 'refunded' ) {?>
+                                <?php } ?>
+                            </li>
+                            <?php if ( current_user_can( 'dokan_manage_order' ) && dokan_get_option( 'order_status_change', 'dokan_selling', 'on' ) == 'on' && $order->get_status() !== 'cancelled' && $order->get_status() !== 'refunded' ): ?>
                                     <form id="dokan-order-status-form" style="display: flex; flex-direction:column;" action="" method="post">
 
                                             <?php
+                                            $counter = 0;
+                                            $prefix = "status_";
                                             foreach ( $statuses as $status => $label ) {
-                                                echo '<button type="button" style="margin: 5px 0;" onfocus="this.classList.toggle(\'dokan-btn-default\');" class="dokan-btn dokan-btn-success" value="' . esc_attr( $status ) . '" ' . selected( $status, 'wc-' . dokan_get_prop( $order, 'status' ), false ) . '>' . esc_html__( $label, 'dokan-lite' ) . '</button>';
+                                                $counter++;
+                                                echo '<button style="margin: 5px 0;" onfocus="this.classList.toggle(\'dokan-btn-default\');" class="dokan-btn dokan-btn-success" value="' . esc_attr( $status ) . '" ' . selected( $status, 'wc-' . dokan_get_prop( $order, 'status' ), false ) . '>' . esc_html__( $label, 'dokan-lite' ) . '</button>';
                                             }
                                             ?>
 
                                         <input type="hidden" name="order_id" value="<?php echo esc_attr( dokan_get_prop( $order, 'id' ) ); ?>">
                                         <input type="hidden" name="action" value="dokan_change_status">
                                         <input type="hidden" name="_wpnonce" value="<?php echo esc_attr( wp_create_nonce( 'dokan_change_status' ) ); ?>">
-                                        <input type="submit" class="dokan-btn dokan-btn-success dokan-btn-sm" style="background-color: blue;" name="dokan_change_status" value="<?php esc_attr_e( 'Update', 'dokan-lite' ); ?>">
 
-                                        <a href="#" class="dokan-btn dokan-btn-default dokan-btn-sm dokan-cancel-status"><?php esc_html_e( 'Cancel', 'dokan-lite' ) ?></a>
                                     </form>
-                                </li>
                             <?php endif ?>
-                            <li>
-                                <button type="button" style="margin: 5px 0;" onfocus="this.classList.toggle(\'dokan-btn-default\');" class="dokan-btn dokan-btn-success">Scan Container</button>
-                            </li>
